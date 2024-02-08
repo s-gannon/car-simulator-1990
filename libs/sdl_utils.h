05e800a49dbd395ff9a2b9ce0c80d613bb90ad59
Spencer Gannon <spencer.gannon@valpo.edu>
2024-02-08 00:00:33 -0600

    Swapped from gcc to clang, added directories

---
+++ libs/sdl_utils.h
@@ -0,0 +1,13 @@
+#ifndef SDL_UTILS_H
+#define SDL_UTILS_H
+
+
+
+#endif
+
+#ifndef SDL_UTILS_SRC
+#define SDL_UTILS_SRC
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,33 @@
+// System includes
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+// SDL2 includes
 #include <SDL2/SDL.h>
-
-void init_sdl(){
-	if(SDL_Init(SDL_INIT_VIDEO)){ // if SDL_Init returns non-zero or != 0
+// Macros
+#define GB_HEIGHT 144
+#define GB_WIDTH  160
+#define INT_SCALE 3
+// Functions
+void init_sdl(int flags){
+	if(SDL_Init(flags)){ // if SDL_Init returns non-zero, run 
         printf("[ERROR] SDL_Init: %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
+}
+/*
+void init_sdl_images(int flags){
+    if(!IMG_Init(flags)){ // if init returns 0, flip and error out
+        printf("[ERROR] IMG_Init: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+}
+*/
+void cleanup_sdl(SDL_Renderer * render, SDL_Window * win){
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
 
 void check_sdl_window(SDL_Window * win){
@@ -19,25 +40,37 @@ void check_sdl_window(SDL_Window * win){
 
 void check_sdl_renderer(SDL_Renderer * render, SDL_Window * win){
     if(!render){
-        SDL_DestroyWindow(win);
         printf("[ERROR] SDL_CreateRenderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
         SDL_Quit();
         exit(EXIT_FAILURE);
     }
 }
 
-void cleanup_sdl(SDL_Renderer * render, SDL_Window * win){
-    SDL_DestroyRenderer(render);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+void check_sdl_surface(SDL_Surface * face, SDL_Renderer * rend, SDL_Window * win){
+    if(!face){
+        printf("[ERROR] SDL_Surface: %s\n", SDL_GetError());
+        cleanup_sdl(rend, win);
+        exit(EXIT_FAILURE);
+    }
 }
 
-int main(){
+uint8_t * get_keyboard_state(void){
+    SDL_PumpEvents();
+    return SDL_GetKeyboardState(NULL);
+}
+
+int main(void){
+    uint8_t * curr_keyboard_state = NULL;
+    uint8_t * prev_keyboard_state = NULL;
+
+    init_sdl(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
 	SDL_Window * window = SDL_CreateWindow(
-        "Keyboard Test",
+        "Car Simulator 1990",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        640, 480,
+        GB_WIDTH * INT_SCALE, GB_HEIGHT * INT_SCALE,
         SDL_WINDOW_SHOWN
     );
 
@@ -46,7 +79,7 @@ int main(){
     SDL_Renderer * renderer = SDL_CreateRenderer(
         window,
         -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+        (SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
     );
 
     check_sdl_renderer(renderer, window);
@@ -60,7 +93,7 @@ int main(){
                     quit = 1;
                     break;
                 case SDL_KEYDOWN:
-                    if(event.key.keysym.sym = SDLK_SPACE)
+                    if(event.key.keysym.sym == SDLK_SPACE)
                         printf("Space bar was pressed\n");
                     break;
                 case SDL_KEYUP:
